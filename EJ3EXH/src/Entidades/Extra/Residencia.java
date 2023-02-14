7abde5b35f7947df601ab692a4c24b7128e43ff3
pablomanchon <114169685+pablomanchon@users.noreply.github.com>
2023-02-13 22:14:01 -0300

    Modifico Residencia

--- EJ3EXH/src/Entidades/Extra/Residencia.java
+++ EJ3EXH/src/Entidades/Extra/Residencia.java
@@ -1,5 +1,32 @@
-package Entidades.Extra;
+package Entidades;
 
-public class Residencia {
-    
+public class Residencia extends Extrahotelero{
+    private boolean descuentos, campoDeportivo;
+
+    public Residencia() {
+    }
+
+    public boolean isDescuentos() {
+        return descuentos;
+    }
+
+    public void setDescuentos(boolean descuentos) {
+        this.descuentos = descuentos;
+    }
+
+    public boolean isCampoDeportivo() {
+        return campoDeportivo;
+    }
+
+    public void setCampoDeportivo(boolean campoDeportivo) {
+        this.campoDeportivo = campoDeportivo;
+    }
+
+    @Override
+    public String toString() {
+        return "Residencia{" +
+                "descuentos=" + descuentos +
+                ", campoDeportivo=" + campoDeportivo +
+                '}';
+    }
 }
