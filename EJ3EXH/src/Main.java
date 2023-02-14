19e7dc08cced93f5eb42ac1027711fe079c49f23
pablomanchon <114169685+pablomanchon@users.noreply.github.com>
2023-02-13 22:56:17 -0300

    prueba ramas

--- EJ3EXH/src/Main.java
+++ EJ3EXH/src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hola Mundo!");
         System.out.println("Rama pablo2");
-        System.out.println("prueba");
+        System.out.println("prueba cambio main");
     }
 }
